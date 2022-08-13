356394d92015163d722257941b8648836c6d194c
nagode.sandip11 <nagode.sandip11@gmail.com>
2022-08-13 12:10:28 +0530

    add xyz cont

---
+++ IdealOnlineBillingNew/Controllers/XYZController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdealOnlineBillingNew.Controllers
+{
+    public class XYZController : Controller
+    {
+        // GET: XYZ
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
