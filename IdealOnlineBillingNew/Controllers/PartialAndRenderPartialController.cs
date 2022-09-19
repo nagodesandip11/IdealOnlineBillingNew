ae3fb20b710f3f4fcac62b8cc8a0cc252d7315f8
nagode.sandip11 <nagode.sandip11@gmail.com>
2022-09-19 20:50:53 +0530

    xv

---
+++ IdealOnlineBillingNew/Controllers/PartialAndRenderPartialController.cs
@@ -0,0 +1,26 @@
+﻿using IdealOnlineBillingNew.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdealOnlineBillingNew.Controllers
+{
+    public class PartialAndRenderPartialController : Controller
+    {
+        IdealWebDB db = new IdealWebDB();
+        // GET: PartialAndRenderPartial
+        public ActionResult Index()
+        {
+            var emp = db.Employees.ToList();
+            // return PartialView("_EmployeeList", emp);
+            return View(emp);
+        }
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
+    }
+}
